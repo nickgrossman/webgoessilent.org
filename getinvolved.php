a03467bf37d022729ce98e0feb467699c3b7f945
Nick Grossman <nick@wrkng.net>
2012-12-19 21:38:22 -0500

    add getinvolved page; move things to php

---
+++ getinvolved.php
@@ -0,0 +1,27 @@
+<?php include('includes/header.php'); ?>
+
+ <center>
+	 <h3>Other ways to get involved:</h3><br />
+ </center>
+
+ <div class="row">
+
+	 <div class="span2 offset1">
+		 <a href="http://www.causes.com/causes/807161-stand-with-sandy-hook/actions/1716727"><img src="img/ribbon-150.png" /></a>
+	 </div>
+	 <div class="span4">
+	 	Add the Moment of Silence badge to your website by visiting <a href="http://www.causes.com/causes/807161-stand-with-sandy-hook/actions/1716727">this campaign on Causes</a>.
+	 </div>
+ </div>
+ <hr />
+ <div class="row">
+ 	 <div class="span2 offset1">
+ 		 <a href="http://demandaplan.org"><img src="img/demandaplan.jpg" /></a>
+ 	 </div>
+ 	 <div class="span4">
+ 	 	Find other ways to get involved at <a href="http://demandaplan.org">Demand a Plan</a>.
+ 	 </div>
+  </div>
+
+
+<?php include('includes/footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php include('includes/header.php'); ?>
+		
+		<div style="text-align:center">
+			<img src="img/ribbon.png?v=2" />
+		
+			<p style="font-size: 1.3em; padding-top: 15px">Friday, December 21, 2012, 9:30am EST |  <a href="https://twitter.com/search?q=%23momentforsandyhook&src=typd">#momentforsandyhook</a></p>
+		</div>
+		
+		<hr />
+
+		<h1>The web goes silent for Sandy Hook</h1>
+		<hr />
+		
+		<h3 id="subhead">Join the web in observing a moment of silence to remember the victims of the Sandy Hook tragedy.</h3>
+		
+		<img id="screenshot" src="img/example2.png" />
+		
+		<p style="text-align:right; color: #ccc; padding-top: 10px; font-size: .9em">Yahoo.com shown above for demonstration purposes only</p>
+		
+		<hr />
+		
+		<center>
+			
+			<h3 style="line-height: 1.4em; margin-bottom: 1em;">Have your website automatically "go silent" <br />during the national moment of silence</h3>
+		
+		<p>Simply install a line of javascript in your website to participate automatically in Friday's moment of silence.</p>
+		<p>Sign up here and we'll let you know when the javascript code is ready.</p>
+		
+		<form id="form19" name="form19" class="form-inline" autocomplete="off" enctype="multipart/form-data" method="post" novalidate
+		action="https://wrkng.wufoo.com/forms/z7x1k9/#public">
+				
+		<label>Website:</label>		
+				
+		<input id="Field2" name="Field2" type="url" class="input field text medium" value="http://" maxlength="255" tabindex="1" placeholder="Website URL" /> 
+		
+		<input id="Field1" name="Field1" type="email" spellcheck="false" class="input field text medium" value="" maxlength="255" tabindex="2" placeholder="Your Email" /> 
+		
+		
+		<button id="saveForm" name="saveForm" 
+type="submit" type="submit" class="btn">Go</button>
+						
+	
+		<input type="hidden" id="idstamp" name="idstamp" value="OZAEcD2w8mf8lV0p6HOP6x3zTtHPBFK62kFI49Q3Uos=" />
+		
+		</form> 
+	 
+ </center>
+ 
+ <hr />
+ 
+ <center>
+	 <h3>Other ways to get involved:</h3><br />
+ </center>
+ 
+ <div class="row">
+	 
+	 <div class="span2 offset1">
+		 <a href="http://www.causes.com/causes/807161-stand-with-sandy-hook/actions/1716727"><img src="img/ribbon-150.png" /></a>
+	 </div>
+	 <div class="span4">
+	 	Add the Moment of Silence badge to your website by visiting <a href="http://www.causes.com/causes/807161-stand-with-sandy-hook/actions/1716727">this campaign on Causes</a>.
+	 </div>
+ </div>
+ <hr />
+ <div class="row">
+ 	 <div class="span2 offset1">
+ 		 <a href="http://demandaplan.org"><img src="img/demandaplan.jpg" /></a>
+ 	 </div>
+ 	 <div class="span4">
+ 	 	Find other ways to get involved at <a href="http://demandaplan.org">Demand a Plan</a>.
+ 	 </div>
+  </div>
+		
+	 
+<?php include('includes/footer.php'); ?>
