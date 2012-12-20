70db608b988f64c60b64a55c876c2b8c3e37c0b9
Nick Grossman <nick@wrkng.net>
2012-12-19 21:41:23 -0500

    adding favicon

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@
   <head>
 	<meta charset="utf-8">
 	<title>The Web Goes Silent for Sandy Hook</title>
+	<link rel="shortcut icon" href="img/favicon.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
